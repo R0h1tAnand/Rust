fc80e96cf373eec888b34d0b9a7f6c7f61de7910
R0h1tAnand <rohitanand9419@gmail.com>
2025-08-04 23:29:22 +0530

    Renamed Folders

---
+++ 01_integers/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Scalar Types: int, float, boolean, char
+    //unsigned - never negative - u8, u16, u32, u64, u128, usize
+    //Signed - can be negative - i8, i16, i32, i64, i128, isize
+    println!("Max Size of u32: {}", u32::MAX);
+    println!("Max Size of u64: {}", u64::MAX);
+    println!("Max Size of i32: {}", i32::MAX);
+    println!("Max Size of i64: {}", i64::MAX);
+
+    //int literals - decimals  10000, binary 111000
+}
