bd974dd778e2d237b28132f66af7849967b976d0
R0h1tAnand <rohitanand9419@gmail.com>
2025-08-04 23:35:18 +0530

    Add Cargo.lock file and update main.rs to demonstrate scalar and float types

--- _02_Floats/src/main.rs
+++ _02_Floats/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // Scalar Types: int, float, boolean, char
+    //unsigned - never negative - u8, u16, u32, u64, u128, usize
+    //Signed - can be negative - i8, i16, i32, i64, i128, isize
+    println!("Max Size of u32: {}", u32::MAX);
+    println!("Max Size of u64: {}", u64::MAX);
+    println!("Max Size of i32: {}", i32::MAX);
+    println!("Max Size of i64: {}", i64::MAX);
+
+    // Floats - f32, >f64< - 3.14
+    println!("Max Size of f32: {}", f32::MAX);
+    println!("Max Size of f64: {}", f64::MAX);
 }
