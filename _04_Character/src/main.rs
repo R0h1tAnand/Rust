b78fea48f6d0fcd7885cba1ebff1f677622ce4c0
R0h1tAnand <rohitanand9419@gmail.com>
2025-08-04 23:44:23 +0530

    Add initial Cargo.toml, Cargo.lock, and main.rs files for _04_Character example

---
+++ _04_Character/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // Scalar Types: int, float, boolean, char
+    //unsigned - never negative - u8, u16, u32, u64, u128, usize
+    //Signed - can be negative - i8, i16, i32, i64, i128, isize
+    println!("Max Size of u32: {}", u32::MAX);
+    println!("Max Size of u64: {}", u64::MAX);
+    println!("Max Size of i32: {}", i32::MAX);
+    println!("Max Size of i64: {}", i64::MAX);
+
+    // Floats - f32, >f64< - 3.14
+    println!("Max Size of f32: {}", f32::MAX);
+    println!("Max Size of f64: {}", f64::MAX);
+
+    // Boolean - true, false - bool
+    // true or false - true, true and false - false
+
+    //character - char - 4 bytes
+    println!("A bus");
+    println!("{}", 'A'); // For printing a character {} is necessary
+}
