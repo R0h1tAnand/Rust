076f71b6865a7817fcaab8c107b7b5878c4b3bed
R0h1tAnand <rohitanand9419@gmail.com>
2025-08-06 13:35:25 +0530

    RUST: Variables And Constants

---
+++ _05_Variables/src/main.rs
@@ -0,0 +1,12 @@
+//Constants or global variables
+const NUMBER: i32 = 17; 
+fn main() {
+    // Variable are immutable
+    let hello = "Hello, world!";
+    //let mut hello = "Hello, World!";
+    println!("{}", hello);
+    let x = 5;
+    let y = 6;
+    println!("Sum of x: {} + y:{} is - {}", x, y, x + y);
+    println!("{}", NUMBER);
+}
