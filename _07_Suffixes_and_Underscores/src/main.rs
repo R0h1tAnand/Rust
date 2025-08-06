d861190bd0b22808400768d9594e1ac50823f7b9
R0h1tAnand <rohitanand9419@gmail.com>
2025-08-06 14:40:11 +0530

    Suffixes and Underscores

---
+++ _07_Suffixes_and_Underscores/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x = 42_u32; // Can also be written as 42u32
+    let y = 1_000_000; // This is a valid way to write large numbers for better readability 
+    println!("x: {}, y: {}", x, y);
+}
