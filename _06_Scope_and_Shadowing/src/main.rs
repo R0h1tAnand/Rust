f456975b7418ab6af3293077492c607c00cdbe11
R0h1tAnand <rohitanand9419@gmail.com>
2025-08-06 13:48:10 +0530

    Scope and Shadowing

---
+++ _06_Scope_and_Shadowing/src/main.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+fn main() {
+    // let x = 1;
+    // {
+    //     let x = 2;
+    //     println!("{}", x);
+    // }
+    // println!("{}", x);
+    let x = 1;
+    let x = "hello";
+    println!("{}", x);
+}
