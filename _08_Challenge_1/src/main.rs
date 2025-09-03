6e792a64cd252ea6ed39bb917bbf12f48f61c38e
R0h1tAnand <rohitanand9419@gmail.com>
2025-09-03 20:09:22 +0530

    Solution For Challenge 1

---
+++ _08_Challenge_1/src/main.rs
@@ -0,0 +1,10 @@
+#![allow(unused)]
+//Global Variable
+const BIRTHDAY: i32 = 1;
+fn main() {
+    let my_name = "Rohit";
+    let my_brithday = "5th January";
+    let mut age = 20;
+    let new_age = age + 1;
+    println!("My name is {}, and I am {} Years Old. I will Turn {} on {}",my_name,age,new_age,my_brithday);
+}
